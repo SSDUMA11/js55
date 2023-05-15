4d528f01e7c0f6093d55e160ef58963d11144143
SSDUMA11 <dumasofia2@gmail.com>
2023-05-15 18:55:29 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, ComponentRef } from '@angular/core';
+import { PopUpComponent } from './pop-up/pop-up.component';
+import { UserGroupService } from './service/user-group.service';
+import { UsersService } from './service/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  providers: [UsersService]
 })
 export class AppComponent {
-  title = 'angular-app';
+  @ViewChild('popUp', { read: ViewContainerRef })
+  private viewRef!: ViewContainerRef;
+  private componentRef!: ComponentRef<PopUpComponent>;
+
+  constructor(public usersService: UsersService, public userGroupService: UserGroupService) {}
+
+  showPopUp() {
+    this.componentRef = this.viewRef.createComponent(PopUpComponent);
+    this.componentRef.instance.groupName = this.userGroupService.getGroupName();
+    this.componentRef.instance.members = this.usersService.getUsers().length;
+
+    
+    this.componentRef.instance.close.subscribe(()=>{this.viewRef.clear()})
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
+import { PopUpComponent } from './pop-up/pop-up.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HeaderComponent,
+    PopUpComponent,
   ],
   imports: [
     BrowserModule
